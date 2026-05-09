7fe0cc789216e065067a651c1a489d3884231566
satish kumar <satishkumardara24@gmail.com>
2026-05-09 23:14:59 +0530

    confusion

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-/*
- <div id="parent">
-        <div id="child">
-            
-            <h1>I'm an h1</h1>
-            
-        </div>
-    </div>
-*/
+// import React from "react";
+// import ReactDOM from "react-dom/client"
+console.log("HELLO SATISH, THIS IS THE RIGHT FILE")
 let parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "I'm an h1"),React.createElement("h2", {}, "I'm an h2")]
-  ),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1"),
+    React.createElement("h2", {}, "I'm an h2"),
+  ]),
 );
 
 let h1 = React.createElement("h1", {}, "satish kumar form React");
